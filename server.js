ac01ffd1fc89753dce814308aed1299435751477
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-09-23 12:04:38 +0200

    Fixed string quotation error

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
+var host = '';//EKSPERYMENT
+var port = '';//EKSPERYMENT
+
 
 app.use('/store', function (req, res, next) {
     console.log('Jestem pośrednikiem dla endpointu \'/store\'');
@@ -9,14 +12,14 @@ app.use('/store', function (req, res, next) {
 });
 
 app.get('/', function(req, res) {
-    console.log('To jest homesite -> console');
-    res.send('To jest homesite (web browser)');    
+    console.log('Redirecting to \'/auth/google\'');
+    res.redirect('/auth/google');
 });
 
 app.get('/first-template', function(req, res) {
     res.render('first-template', {
-        name: "Carribean Queen",
-        url: "http://carribeanqueen.nl",
+        name: 'Carribean Queen',
+        url: 'http://carribeanqueen.nl',
         user: {
             name: 'Johnny',
             age: 20
@@ -44,10 +47,19 @@ app.get('/store', function(req, res) {
 });
 
 var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Aplikacja nasłuchuje na http://' + host + ':' + port);
+    host = server.address().address;
+    port = server.address().port;
+    console.log('2. Aplikacja nasłuchuje na http://' + host + ':' + port);
 });
+
+// EXPERYMENT
+console.log('1. Aplikacja nasłuchuje na http://' + host + ':' + port);
+
+
+// EKPERYMENT
+setTimeout(function() {
+    console.log('3. Aplikacja nasłuchuje na http://' + host + ':' + port);}
+, 2);//PRZY 1MS FUNKCJA NIE WSTAWIA ZMIENNYCH
 
 app.use(function(req, res, next) {
     res.status(404).send('Very Fatal Error (code: ' + res.statusCode + '), everything gone very bad');
